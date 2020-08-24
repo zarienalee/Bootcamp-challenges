afd1f220c56016e83bf4167b3f32e02a076268e4
zarienalee <zariena5@gmail.com>
2020-08-24 13:10:08 +0200

    Corrections

--- Task-2.js
+++ Task-2.js
@@ -2,5 +2,5 @@ var x = 1 + 1 * 2;
 var y = (1 + 1) * 2;
 var z = 1 + 1 * 2;
 var a = 1 + (1 * 2) / 2;
-var b = (1 + 1 * 2) / 2;
+var b = (1 + 1 * 2) / 2; 
 console.log(x);
--- Task-4.js
+++ Task-4.js
@@ -1,10 +1,10 @@
 function checkThree(val1, val2) {
   var sum = val1 + val2;
-  JSON.stringify(sum);
-  if (val1 == 3 || (val2 == 3 && sum.includes("3"))) {
+  JSON.stringify(sum)
+  if (val1 == 3 || val2 == 3 && sum.includes('3')) {
     return true;
   } else {
     return false;
   }
 }
-console.log(checkThree(3, 2));
+console.log(checkThree(13,12)); 
